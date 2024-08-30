6e0aa3b3bae6404bd689e2b2d1d5da9ce8647ebc
unknown <perezlozanoalanandres@gmail.com>
2024-08-29 19:23:56 -0600

    Act settings

--- app/(tabs)/settings.jsx
+++ app/(tabs)/settings.jsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet,Text, View, ViewComponent} from "react-native";
+import { StyleSheet,Text, View} from "react-native";
 
 const Settings =() =>{
     return(
