b412258c793932ed4467864d87ef0a816c6d43f1
Roberto RG <robertoromeroguadarrama@gmail.com>
2024-09-02 20:09:51 -0600

    pantalla crear

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,9 +1,32 @@
 import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const TabLayout = () => {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "inicio" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "inicio",
+          tabBarIcon: () => <Feather name="home" size={24} />,
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Crear",
+          tabBarIcon: () => <FontAwesome6 name="add" size={24} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Configuracion",
+          tabBarIcon: () => <MaterialIcons name="settings" size={24} />,
+        }}
+      />
     </Tabs>
   );
 };
